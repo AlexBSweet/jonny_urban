b502aab45d7cc4e687166eca25f275b12b0f62fd
AlexBSweet <45742179+AlexBSweet@users.noreply.github.com>
2021-01-13 21:37:44 -0500

    updates

--- mobile.js
+++ mobile.js
@@ -33,13 +33,124 @@ let pixels
 
 
 
+const mobile = window.matchMedia('(max-width: 900px)')
+const desktop = window.matchMedia('(min-width:900px')
+
+
+
+// DESKTOP POSITIONING FUNCTIONS=====================================
+
+const desktopPink = ()=>{
+    pinkSection.classList.add('desktop-1')
+    pinkSection.classList.remove('desktop-3')
+
+    blueSection.classList.add('desktop-2')
+    blueSection.classList.remove('desktop-1')
+    blueSection.classList.remove('desktop-3')
+
+    tealSection.classList.add('desktop-3')
+    tealSection.classList.remove('desktop-2')
+    tealSection.classList.remove('desktop-1')
+
+    whiteSection.classList.add('desktop-3')
+    whiteSection.classList.remove('desktop-2')
+    whiteSection.classList.remove('desktop-1')
+
+    graySection.classList.add('desktop-3')
+    graySection.classList.remove('desktop-2')
+    graySection.classList.remove('desktop-1')
+}
+
+const desktopBlue = ()=>{
+    pinkSection.classList.add('desktop-3')
+    pinkSection.classList.remove('desktop-1')
+
+    blueSection.classList.add('desktop-1')
+    blueSection.classList.remove('desktop-2')
+    blueSection.classList.remove('desktop-3')
+
+    tealSection.classList.add('desktop-2')
+    tealSection.classList.remove('desktop-3')
+    tealSection.classList.remove('desktop-1')
+
+    whiteSection.classList.add('desktop-3')
+    whiteSection.classList.remove('desktop-2')
+    whiteSection.classList.remove('desktop-1')
+
+    graySection.classList.add('desktop-3')
+    graySection.classList.remove('desktop-2')
+    graySection.classList.remove('desktop-1')
+}
+
+const desktopTeal = ()=>{
+    pinkSection.classList.add('desktop-3')
+    pinkSection.classList.remove('desktop-1')
+
+    blueSection.classList.add('desktop-3')
+    blueSection.classList.remove('desktop-1')
+    blueSection.classList.remove('desktop-2')
+
+    tealSection.classList.add('desktop-1')
+    tealSection.classList.remove('desktop-2')
+    tealSection.classList.remove('desktop-3')
+
+    whiteSection.classList.add('desktop-2')
+    whiteSection.classList.remove('desktop-3')
+    whiteSection.classList.remove('desktop-1')
+
+    graySection.classList.add('desktop-3')
+    graySection.classList.remove('desktop-2')
+    graySection.classList.remove('desktop-1')
+}
+
+const desktopWhite = ()=>{
+    pinkSection.classList.add('desktop-3')
+    pinkSection.classList.remove('desktop-1')
+
+    blueSection.classList.add('desktop-3')
+    blueSection.classList.remove('desktop-1')
+    blueSection.classList.remove('desktop-2')
+
+    tealSection.classList.add('desktop-3')
+    tealSection.classList.remove('desktop-1')
+    tealSection.classList.remove('desktop-2')
+
+    whiteSection.classList.add('desktop-1')
+    whiteSection.classList.remove('desktop-3')
+    whiteSection.classList.remove('desktop-2')
+
+    graySection.classList.add('desktop-2')
+    graySection.classList.remove('desktop-3')
+    graySection.classList.remove('desktop-1')
+}
+
+const desktopGray = ()=>{
+    pinkSection.classList.add('desktop-3')
+    pinkSection.classList.remove('desktop-1')
+
+    blueSection.classList.add('desktop-3')
+    blueSection.classList.remove('desktop-1')
+    blueSection.classList.remove('desktop-2')
+
+    tealSection.classList.add('desktop-3')
+    tealSection.classList.remove('desktop-1')
+    tealSection.classList.remove('desktop-2')
+
+    whiteSection.classList.add('desktop-2')
+    whiteSection.classList.remove('desktop-3')
+    whiteSection.classList.remove('desktop-1')
+
+    graySection.classList.add('desktop-1')
+    graySection.classList.remove('desktop-3')
+    graySection.classList.remove('desktop-2')
+}
 
 
 
 
 
 
-const mobileFunctions = ()=>{
+const allFunctions = ()=>{
     document.addEventListener('scroll', ()=>{
         pixels = window.pageYOffset
     
@@ -80,162 +191,202 @@ const mobileFunctions = ()=>{
     })
     
     
-    
+
     pinkSectionHeader.addEventListener('click', ()=>{
+        if(mobile.matches){
+            pinkSection.classList.add('open')
+            pinkInner.classList.remove('hidden')
+            pinkSectionHeader.classList.add('section-open')
         
+            blueSection.classList.remove('open')
+            blueSectionHeader.classList.remove('section-open')
+            blueInner.classList.add('hidden')
+        
+            tealSection.classList.remove('open')
+            tealSectionHeader.classList.remove('section-open')
+            tealInner.classList.add('hidden')
+        
+            whiteSection.classList.remove('open')
+            whiteSectionHeader.classList.remove('section-open')
+            whiteInner.classList.add('hidden')
+        
+            graySection.classList.remove('open')
+            graySectionHeader.classList.remove('section-open')
+            grayInner.classList.add('hidden')
+        
+            pinkSection.scrollIntoView({behavior: "smooth", block:"start"})
+        }
+       
     
-        pinkSection.classList.add('open')
-        pinkInner.classList.remove('hidden')
-        pinkSectionHeader.classList.add('section-open')
-    
-        blueSection.classList.remove('open')
-        blueSectionHeader.classList.remove('section-open')
-        blueInner.classList.add('hidden')
-    
-        tealSection.classList.remove('open')
-        tealSectionHeader.classList.remove('section-open')
-        tealInner.classList.add('hidden')
-    
-        whiteSection.classList.remove('open')
-        whiteSectionHeader.classList.remove('section-open')
-        whiteInner.classList.add('hidden')
-    
-        graySection.classList.remove('open')
-        graySectionHeader.classList.remove('section-open')
-        grayInner.classList.add('hidden')
-    
-        pinkSection.scrollIntoView({behavior: "smooth", block:"start"})
+    })
+
+    pinkSection.addEventListener('click', ()=>{
+        if(desktop.matches){
+            desktopPink()
+        }
     })
     
     blueSectionHeader.addEventListener('click', ()=>{
+        if(mobile.matches){
+            pinkSection.classList.remove('open')
+            pinkSectionHeader.classList.remove('section-open')
+            pinkInner.classList.add('hidden')
+        
+            blueSection.classList.add('open')
+            blueInner.classList.remove('hidden')
+            blueSectionHeader.classList.add('section-open')
+        
+            tealSection.classList.remove('open')
+            tealSectionHeader.classList.remove('section-open')
+            tealInner.classList.add('hidden')
+        
+            whiteSection.classList.remove('open')
+            whiteSectionHeader.classList.remove('section-open')
+            whiteInner.classList.add('hidden')
+        
+            graySection.classList.remove('open')
+            graySectionHeader.classList.remove('section-open')
+            grayInner.classList.add('hidden')
+        
+            blueSection.scrollIntoView({behavior: "smooth"})
+        }
     
-        pinkSection.classList.remove('open')
-        pinkSectionHeader.classList.remove('section-open')
-        pinkInner.classList.add('hidden')
-    
-        blueSection.classList.add('open')
-        blueInner.classList.remove('hidden')
-        blueSectionHeader.classList.add('section-open')
-    
-        tealSection.classList.remove('open')
-        tealSectionHeader.classList.remove('section-open')
-        tealInner.classList.add('hidden')
-    
-        whiteSection.classList.remove('open')
-        whiteSectionHeader.classList.remove('section-open')
-        whiteInner.classList.add('hidden')
-    
-        graySection.classList.remove('open')
-        graySectionHeader.classList.remove('section-open')
-        grayInner.classList.add('hidden')
-    
-        blueSection.scrollIntoView({behavior: "smooth"})
-    
+    })
+
+    blueSection.addEventListener('click', ()=>{
+        if(desktop.matches){
+            desktopBlue()
+        }
     })
     
     tealSection.addEventListener('click', ()=>{
     
-        pinkSection.classList.remove('open')
-        pinkSectionHeader.classList.remove('section-open')
-        pinkInner.classList.add('hidden')
-    
-        blueSection.classList.remove('open')
-        blueSectionHeader.classList.remove('section-open')
-        blueInner.classList.add('hidden')
-    
+        if(mobile.matches){
+            pinkSection.classList.remove('open')
+            pinkSectionHeader.classList.remove('section-open')
+            pinkInner.classList.add('hidden')
         
-        tealSection.classList.add('open')
-        tealInner.classList.remove('hidden')
-        tealSectionHeader.classList.add('section-open')
-    
-        whiteSection.classList.remove('open')
-        whiteSectionHeader.classList.remove('section-open')
-        whiteInner.classList.add('hidden')
-    
-        graySection.classList.remove('open')
-        graySectionHeader.classList.remove('section-open')
-        grayInner.classList.add('hidden')
-    
-        tealSection.scrollIntoView({behavior: "smooth"})
+            blueSection.classList.remove('open')
+            blueSectionHeader.classList.remove('section-open')
+            blueInner.classList.add('hidden')
+        
+            
+            tealSection.classList.add('open')
+            tealInner.classList.remove('hidden')
+            tealSectionHeader.classList.add('section-open')
+        
+            whiteSection.classList.remove('open')
+            whiteSectionHeader.classList.remove('section-open')
+            whiteInner.classList.add('hidden')
+        
+            graySection.classList.remove('open')
+            graySectionHeader.classList.remove('section-open')
+            grayInner.classList.add('hidden')
+        
+            tealSection.scrollIntoView({behavior: "smooth"})
+        }
+
+        
     })
     
+    tealSection.addEventListener('click', ()=>{
+        if(desktop.matches){
+            desktopTeal()
+        }
+    })
     
     whiteSection.addEventListener('click', ()=>{
     
-        pinkSection.classList.remove('open')
-        pinkSectionHeader.classList.remove('section-open')
-        pinkInner.classList.add('hidden')
-    
-        blueSection.classList.remove('open')
-        blueSectionHeader.classList.remove('section-open')
-        blueInner.classList.add('hidden')
-    
-        tealSection.classList.remove('open')
-        tealSectionHeader.classList.remove('section-open')
-        tealInner.classList.add('hidden')
-    
+        if(mobile.matches){
+            pinkSection.classList.remove('open')
+            pinkSectionHeader.classList.remove('section-open')
+            pinkInner.classList.add('hidden')
         
-        whiteSection.classList.add('open')
-        whiteInner.classList.remove('hidden')
-        whiteSectionHeader.classList.add('section-open')
-    
-        graySection.classList.remove('open')
-        graySectionHeader.classList.remove('section-open')
-        grayInner.classList.add('hidden')
+            blueSection.classList.remove('open')
+            blueSectionHeader.classList.remove('section-open')
+            blueInner.classList.add('hidden')
         
-        whiteSection.scrollIntoView({behavior: "smooth"})
+            tealSection.classList.remove('open')
+            tealSectionHeader.classList.remove('section-open')
+            tealInner.classList.add('hidden')
+        
+            
+            whiteSection.classList.add('open')
+            whiteInner.classList.remove('hidden')
+            whiteSectionHeader.classList.add('section-open')
+        
+            graySection.classList.remove('open')
+            graySectionHeader.classList.remove('section-open')
+            grayInner.classList.add('hidden')
+            
+            whiteSection.scrollIntoView({behavior: "smooth"})
+        }
+        
+    })
+
+    whiteSection.addEventListener('click', ()=>{
+        if(desktop.matches){
+            desktopWhite()
+        }
     })
     
     
     graySection.addEventListener('click', ()=>{
-    
-        pinkSection.classList.remove('open')
-        pinkSectionHeader.classList.remove('section-open')
-        pinkInner.classList.add('hidden')
-    
-        blueSection.classList.remove('open')
-        blueSectionHeader.classList.remove('section-open')
-        blueInner.classList.add('hidden')
-    
-        tealSection.classList.remove('open')
-        tealSectionHeader.classList.remove('section-open')
-        tealInner.classList.add('hidden')
-    
-        whiteSection.classList.remove('open')
-        whiteSectionHeader.classList.remove('section-open')
-        whiteInner.classList.add('hidden')
         
-        graySection.classList.add('open')
-        grayInner.classList.remove('hidden')
-        graySectionHeader.classList.add('section-open')
-        graySection.scrollIntoView({behavior: "smooth", block:"start"})
+        if(mobile.matches){
+            pinkSection.classList.remove('open')
+            pinkSectionHeader.classList.remove('section-open')
+            pinkInner.classList.add('hidden')
+        
+            blueSection.classList.remove('open')
+            blueSectionHeader.classList.remove('section-open')
+            blueInner.classList.add('hidden')
+        
+            tealSection.classList.remove('open')
+            tealSectionHeader.classList.remove('section-open')
+            tealInner.classList.add('hidden')
+        
+            whiteSection.classList.remove('open')
+            whiteSectionHeader.classList.remove('section-open')
+            whiteInner.classList.add('hidden')
+            
+            graySection.classList.add('open')
+            grayInner.classList.remove('hidden')
+            graySectionHeader.classList.add('section-open')
+            graySection.scrollIntoView({behavior: "smooth", block:"start"})
+        } 
+       
+
+    })
+
+    graySection.addEventListener('click', ()=>{
+        if(desktop.matches){
+            desktopGray()
+        }
     })
     
 }
 
 
+allFunctions()
 
 
 
 
-
-const mobile = window.matchMedia('(max-width: 900px)')
-const desktop = window.matchMedia('(min-width:900px')
-
-if (mobile.matches) {
-    mobileFunctions()
-  } else if (desktop.matches){
-   //   desktop code will go here, do not want mobile function to keep running once we expand to desktop size
-  }
+// if (mobile.matches) {
+//     mobileFunctions()
+//   } else if (desktop.matches){
+//    //   desktop code will go here, do not want mobile function to keep running once we expand to desktop size
+//   }
 
 
-mobile.onchange = function(){
-    if (mobile.matches) {
-        mobileFunctions()
-      } else if (desktop.matches){
-        //   desktop code will go here, do not want mobile function to keep running once we expand to desktop size
-      }
-}
+// mobile.onchange = function(){
+//     if (mobile.matches) {
+//         mobileFunctions()
+//       } else if (desktop.matches){
+//         //   desktop code will go here, do not want mobile function to keep running once we expand to desktop size
+//       }
+// }
 
 
 
